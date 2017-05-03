3680c6f8b1748be1232d4b7ca8c351431bf455fb
nicolebldf <nicole.baldauf.nb@gmail.com>
2017-05-03 10:27:42 +0200

    Locations

---
+++ locations.js
@@ -0,0 +1,842 @@
+var data = [
+	{
+		"location_name": "Akademia Training Club",
+		"Area Code": "C",
+		"location_type": "Fitness",
+		"postcode": "1094PW"
+	},
+	{
+		"location_name": "Amsfit",
+		"Area Code": "D",
+		"location_type": "Fitness",
+		"postcode": "1054ES"
+	},
+	{
+		"location_name": "Amsterdam Training Center",
+		"Area Code": "C",
+		"location_type": "Vechtsport",
+		"postcode": "1096AN"
+	},
+	{
+		"location_name": "Amsterdam Yoga Centre",
+		"Area Code": "A",
+		"location_type": "Yoga",
+		"postcode": "1016AD"
+	},
+	{
+		"location_name": "ApenkooiGym",
+		"Area Code": "A",
+		"location_type": "Overig",
+		"postcode": "1016SL"
+	},
+	{
+		"location_name": "Apollo Health & Fitness",
+		"Area Code": "E",
+		"location_type": "Fitness",
+		"postcode": "1077BA"
+	},
+	{
+		"location_name": "ArenaGym",
+		"Area Code": "F",
+		"location_type": "Fitness",
+		"postcode": "1101CD"
+	},
+	{
+		"location_name": "Asics Running Clinc",
+		"Area Code": "D",
+		"location_type": "Overig",
+		"postcode": "1054ES"
+	},
+	{
+		"location_name": "B Gym",
+		"Area Code": "D",
+		"location_type": "Vechtsport",
+		"postcode": "1066VH"
+	},
+	{
+		"location_name": "BarreConcept Nederland",
+		"Area Code": "A",
+		"location_type": "Dans",
+		"postcode": "1015NG"
+	},
+	{
+		"location_name": "Bijlmer Sportcentrum",
+		"Area Code": "F",
+		"location_type": "Overig",
+		"postcode": "1102DR"
+	},
+	{
+		"location_name": "Bluebirds Yoga",
+		"Area Code": "A",
+		"location_type": "Yoga",
+		"postcode": "1018MS"
+	},
+	{
+		"location_name": "Body Release Training",
+		"Area Code": "D",
+		"location_type": "Fitness",
+		"postcode": "1064LG"
+	},
+	{
+		"location_name": "Boksschool Bijlmerbajes",
+		"Area Code": "C",
+		"location_type": "Vechtsport",
+		"postcode": "1096AN"
+	},
+	{
+		"location_name": "Boostcamp Amsterdam",
+		"Area Code": "E",
+		"location_type": "Bootcamp",
+		"postcode": "1075"
+	},
+	{
+		"location_name": "Boxxzuid.nl",
+		"Area Code": "D",
+		"location_type": "Vechtsport",
+		"postcode": "1054GC"
+	},
+	{
+		"location_name": "Buenting Sport",
+		"Area Code": "E",
+		"location_type": "Fitness",
+		"postcode": "1085RE"
+	},
+	{
+		"location_name": "Caland Fit",
+		"Area Code": "D",
+		"location_type": "Fitness",
+		"postcode": "1068RR"
+	},
+	{
+		"location_name": "Calisthenics Amsterdam",
+		"Area Code": "A",
+		"location_type": "Overig",
+		"postcode": "1018"
+	},
+	{
+		"location_name": "Chalans Amsterdam",
+		"Area Code": "C",
+		"location_type": "Dans",
+		"postcode": "1093JX"
+	},
+	{
+		"location_name": "Chasse Dance Studios",
+		"Area Code": "D",
+		"location_type": "Dans",
+		"postcode": "1057JJ"
+	},
+	{
+		"location_name": "ClubMarnix",
+		"Area Code": "A",
+		"location_type": "Fitness",
+		"postcode": "1015ZN"
+	},
+	{
+		"location_name": "ClubWest",
+		"Area Code": "D",
+		"location_type": "Fitness",
+		"postcode": "1067SM"
+	},
+	{
+		"location_name": "Core Active",
+		"Area Code": "D",
+		"location_type": "Fitness",
+		"postcode": "1058AG"
+	},
+	{
+		"location_name": "CrossFit aan 't IJ",
+		"Area Code": "B",
+		"location_type": "CrossFit",
+		"postcode": "1033SN"
+	},
+	{
+		"location_name": "CrossFit NDSM",
+		"Area Code": "B",
+		"location_type": "CrossFit",
+		"postcode": "1033SC"
+	},
+	{
+		"location_name": "CrossFit Zeeburg",
+		"Area Code": "C",
+		"location_type": "CrossFit",
+		"postcode": "1019AJ"
+	},
+	{
+		"location_name": "Dans Dansen",
+		"Area Code": "E",
+		"location_type": "Dans",
+		"postcode": "1074HP"
+	},
+	{
+		"location_name": "De Bewustzijn School",
+		"Area Code": "E",
+		"location_type": "Yoga",
+		"postcode": "1073GR"
+	},
+	{
+		"location_name": "De Gymzaal ",
+		"Area Code": "D",
+		"location_type": "Fitness",
+		"postcode": "1056VB"
+	},
+	{
+		"location_name": "De Nieuwe Yogaschool",
+		"Area Code": "A",
+		"location_type": "Yoga",
+		"postcode": "1016PL"
+	},
+	{
+		"location_name": "Duofit",
+		"Area Code": "C",
+		"location_type": "Fitness",
+		"postcode": "1098NJ"
+	},
+	{
+		"location_name": "Equal Yoga",
+		"Area Code": "C",
+		"location_type": "Yoga",
+		"postcode": "1092CL"
+	},
+	{
+		"location_name": "Ettaki Gym",
+		"Area Code": "D",
+		"location_type": "Vechtsport",
+		"postcode": "1053VJ"
+	},
+	{
+		"location_name": "Femme Gym",
+		"Area Code": "D",
+		"location_type": "Fitness",
+		"postcode": "1056DV"
+	},
+	{
+		"location_name": "Fit aan 't IJ",
+		"Area Code": "C",
+		"location_type": "Fitness",
+		"postcode": "1019LA"
+	},
+	{
+		"location_name": "Fit Earth",
+		"Area Code": "C",
+		"location_type": "Fitness",
+		"postcode": "1087DP"
+	},
+	{
+		"location_name": "Fitness Center IJburg",
+		"Area Code": "C",
+		"location_type": "Fitness",
+		"postcode": "1087GN"
+	},
+	{
+		"location_name": "Fitness Zuidas",
+		"Area Code": "E",
+		"location_type": "Fitness",
+		"postcode": "1082VW"
+	},
+	{
+		"location_name": "Fitness365 Amsterdam",
+		"Area Code": "F",
+		"location_type": "Fitness",
+		"postcode": "1105AN"
+	},
+	{
+		"location_name": "Flow-Motion",
+		"Area Code": "D",
+		"location_type": "Bootcamp",
+		"postcode": "1054KA"
+	},
+	{
+		"location_name": "Forty Degrees Hot Yoga Amsterdam",
+		"Area Code": "D",
+		"location_type": "Yoga",
+		"postcode": "1054HL"
+	},
+	{
+		"location_name": "Fresh Fitness",
+		"Area Code": "E",
+		"location_type": "Fitness",
+		"postcode": "1073RW"
+	},
+	{
+		"location_name": "Full of Life Diemen",
+		"Area Code": "C",
+		"location_type": "Fitness",
+		"postcode": "1112XJ"
+	},
+	{
+		"location_name": "Fysio Buitenveldert",
+		"Area Code": "E",
+		"location_type": "Yoga",
+		"postcode": "1082GT"
+	},
+	{
+		"location_name": "Golfschool Fairway",
+		"Area Code": "F",
+		"location_type": "Overig",
+		"postcode": "1105AA"
+	},
+	{
+		"location_name": "Great",
+		"Area Code": "E",
+		"location_type": "Fitness",
+		"postcode": "1071XH"
+	},
+	{
+		"location_name": "Great Yoga",
+		"Area Code": "E",
+		"location_type": "Yoga",
+		"postcode": "1071XH"
+	},
+	{
+		"location_name": "Gustav Gym",
+		"Area Code": "E",
+		"location_type": "Fitness",
+		"postcode": "1082MA"
+	},
+	{
+		"location_name": "Gym3",
+		"Area Code": "#N/A",
+		"location_type": "Vechtsport",
+		"postcode": "1185ZZ"
+	},
+	{
+		"location_name": "Happy Yoga Time",
+		"Area Code": "A",
+		"location_type": "Yoga",
+		"postcode": "1016VZ"
+	},
+	{
+		"location_name": "Health Club Jordaan",
+		"Area Code": "A",
+		"location_type": "Fitness",
+		"postcode": "1016PC"
+	},
+	{
+		"location_name": "Health Club The Base",
+		"Area Code": "#N/A",
+		"location_type": "Fitness",
+		"postcode": "1118CL"
+	},
+	{
+		"location_name": "Healthclub Open Air Amsterdam",
+		"Area Code": "#N/A",
+		"location_type": "Bootcamp",
+		"postcode": ""
+	},
+	{
+		"location_name": "Het Gymlokaal",
+		"Area Code": "D",
+		"location_type": "Fitness",
+		"postcode": "1056AB"
+	},
+	{
+		"location_name": "Het Marnix",
+		"Area Code": "A",
+		"location_type": "Overig",
+		"postcode": "1015ZN"
+	},
+	{
+		"location_name": "Het Sportlokaal van De Fysio Man",
+		"Area Code": "D",
+		"location_type": "Bootcamp",
+		"postcode": "1056CB"
+	},
+	{
+		"location_name": "Hot Flow Yoga Amsterdam",
+		"Area Code": "E",
+		"location_type": "Yoga",
+		"postcode": "1075CS"
+	},
+	{
+		"location_name": "Hot Flow Yoga Jordaan",
+		"Area Code": "A",
+		"location_type": "Yoga",
+		"postcode": "1016VR"
+	},
+	{
+		"location_name": "Hot Flow Yoga Rivierenbuurt",
+		"Area Code": "E",
+		"location_type": "Yoga",
+		"postcode": "1078AP"
+	},
+	{
+		"location_name": "Hotpod Yoga",
+		"Area Code": "D",
+		"location_type": "Yoga",
+		"postcode": "1051KM"
+	},
+	{
+		"location_name": "I Love 2 Workout",
+		"Area Code": "E",
+		"location_type": "Bootcamp",
+		"postcode": "1072LL"
+	},
+	{
+		"location_name": "iLoveWorkingOut",
+		"Area Code": "D",
+		"location_type": "Bootcamp",
+		"postcode": "1054KA"
+	},
+	{
+		"location_name": "Iyengar Yoga Centrum Amsterdam",
+		"Area Code": "A",
+		"location_type": "Yoga",
+		"postcode": "1016GL"
+	},
+	{
+		"location_name": "Iyengar Yogaschool Amsterdam",
+		"Area Code": "D",
+		"location_type": "Yoga",
+		"postcode": "1058AD"
+	},
+	{
+		"location_name": "JSX Style studio Oost",
+		"Area Code": "C",
+		"location_type": "Dans",
+		"postcode": "1092EP"
+	},
+	{
+		"location_name": "Karen Clark Body & Mind",
+		"Area Code": "E",
+		"location_type": "Yoga",
+		"postcode": "1074XE"
+	},
+	{
+		"location_name": "Kettlebell Outdoor",
+		"Area Code": "D",
+		"location_type": "Bootcamp",
+		"postcode": "1054GL"
+	},
+	{
+		"location_name": "Krav Maga HSW (Amsterdam)",
+		"Area Code": "#N/A",
+		"location_type": "Vechtsport",
+		"postcode": "2288AA"
+	},
+	{
+		"location_name": "Leef Yoga",
+		"Area Code": "C",
+		"location_type": "Yoga",
+		"postcode": "1091KB"
+	},
+	{
+		"location_name": "Legendary Sports",
+		"Area Code": "D",
+		"location_type": "Fitness",
+		"postcode": "1066AG"
+	},
+	{
+		"location_name": "Live Yoga",
+		"Area Code": "C",
+		"location_type": "Yoga",
+		"postcode": "1094EJ"
+	},
+	{
+		"location_name": "M&M Stand Up Paddling ",
+		"Area Code": "C",
+		"location_type": "Overig",
+		"postcode": "1087HP"
+	},
+	{
+		"location_name": "Medico Vision Sportstraining",
+		"Area Code": "E",
+		"location_type": "Fitness",
+		"postcode": "1076DE"
+	},
+	{
+		"location_name": "Mountain Network Amsterdam",
+		"Area Code": "D",
+		"location_type": "Fitness",
+		"postcode": "1061MD"
+	},
+	{
+		"location_name": "MOVE//WEST",
+		"Area Code": "D",
+		"location_type": "Dans",
+		"postcode": "1053RT"
+	},
+	{
+		"location_name": "MT Fitness",
+		"Area Code": "C",
+		"location_type": "Fitness",
+		"postcode": "1096GL"
+	},
+	{
+		"location_name": "New York Gym",
+		"Area Code": "C",
+		"location_type": "Fitness",
+		"postcode": "1019BL"
+	},
+	{
+		"location_name": "Nicole Coaching",
+		"Area Code": "D",
+		"location_type": "Vechtsport",
+		"postcode": "1054GC"
+	},
+	{
+		"location_name": "Padel Dam",
+		"Area Code": "D",
+		"location_type": "Overig",
+		"postcode": "1067MC"
+	},
+	{
+		"location_name": "Palestra Sports",
+		"Area Code": "D",
+		"location_type": "Fitness",
+		"postcode": "1062AK"
+	},
+	{
+		"location_name": "Parc Studio",
+		"Area Code": "E",
+		"location_type": "Yoga",
+		"postcode": "1073CX"
+	},
+	{
+		"location_name": "Pearl Pilates",
+		"Area Code": "A",
+		"location_type": "Yoga",
+		"postcode": "1018XM"
+	},
+	{
+		"location_name": "PLTS",
+		"Area Code": "D",
+		"location_type": "Yoga",
+		"postcode": "1053LA"
+	},
+	{
+		"location_name": "Pole Dance Factory Amsterdam - Noord",
+		"Area Code": "B",
+		"location_type": "Dans",
+		"postcode": "1033MP"
+	},
+	{
+		"location_name": "Pole Dance Factory Amsterdam- Oost",
+		"Area Code": "C",
+		"location_type": "Dans",
+		"postcode": "1096BD"
+	},
+	{
+		"location_name": "Pop Up Yogaschool",
+		"Area Code": "D",
+		"location_type": "Yoga",
+		"postcode": "1052BA"
+	},
+	{
+		"location_name": "Powerbox Amsterdam",
+		"Area Code": "C",
+		"location_type": "CrossFit",
+		"postcode": "1096AN"
+	},
+	{
+		"location_name": "Rasalila",
+		"Area Code": "D",
+		"location_type": "Yoga",
+		"postcode": "1053SB"
+	},
+	{
+		"location_name": "Ready 2 Rumble",
+		"Area Code": "E",
+		"location_type": "Vechtsport",
+		"postcode": "1072XV"
+	},
+	{
+		"location_name": "Reebok CrossFit 020",
+		"Area Code": "F",
+		"location_type": "CrossFit",
+		"postcode": "1101BA"
+	},
+	{
+		"location_name": "Runnergy",
+		"Area Code": "D",
+		"location_type": "Overig",
+		"postcode": "1057CX"
+	},
+	{
+		"location_name": "Running Holland",
+		"Area Code": "#N/A",
+		"location_type": "Overig",
+		"postcode": "1000AA"
+	},
+	{
+		"location_name": "Salin Health & Yoga",
+		"Area Code": "#N/A",
+		"location_type": "Yoga",
+		"postcode": ""
+	},
+	{
+		"location_name": "Samana Yoga Center",
+		"Area Code": "A",
+		"location_type": "Yoga",
+		"postcode": "1013GG"
+	},
+	{
+		"location_name": "Shape All In",
+		"Area Code": "D",
+		"location_type": "Fitness",
+		"postcode": "1051HE"
+	},
+	{
+		"location_name": "Sloterparkbad",
+		"Area Code": "D",
+		"location_type": "Zwemmen",
+		"postcode": "1064GW"
+	},
+	{
+		"location_name": "Spa Zuiver",
+		"Area Code": "E",
+		"location_type": "Fitness",
+		"postcode": "1081KH"
+	},
+	{
+		"location_name": "Splash",
+		"Area Code": "A",
+		"location_type": "Fitness",
+		"postcode": "1017PH"
+	},
+	{
+		"location_name": "Sport Natural",
+		"Area Code": "B",
+		"location_type": "Fitness",
+		"postcode": "1022LD"
+	},
+	{
+		"location_name": "Sportcentrum Pleizier",
+		"Area Code": "F",
+		"location_type": "Fitness",
+		"postcode": "1106MH"
+	},
+	{
+		"location_name": "Sportcentrum Universum",
+		"Area Code": "C",
+		"location_type": "Fitness",
+		"postcode": "1098XH"
+	},
+	{
+		"location_name": "Sportfondsenbad Oost",
+		"Area Code": "C",
+		"location_type": "Zwemmen",
+		"postcode": "1093NB"
+	},
+	{
+		"location_name": "Sportplaza Mercator",
+		"Area Code": "D",
+		"location_type": "Fitness",
+		"postcode": "1056CB"
+	},
+	{
+		"location_name": "Sportquest",
+		"Area Code": "E",
+		"location_type": "Fitness",
+		"postcode": "1071XK"
+	},
+	{
+		"location_name": "Sports World Amsterdam",
+		"Area Code": "C",
+		"location_type": "Fitness",
+		"postcode": "1093KS"
+	},
+	{
+		"location_name": "Sportschool Nique",
+		"Area Code": "E",
+		"location_type": "Fitness",
+		"postcode": "1079LD"
+	},
+	{
+		"location_name": "Sportschool van de Berg",
+		"Area Code": "D",
+		"location_type": "Fitness",
+		"postcode": "1056KR"
+	},
+	{
+		"location_name": "Squash City",
+		"Area Code": "A",
+		"location_type": "Fitness",
+		"postcode": "1013DE"
+	},
+	{
+		"location_name": "Studio 191",
+		"Area Code": "E",
+		"location_type": "Yoga",
+		"postcode": "1074TV"
+	},
+	{
+		"location_name": "Studio Anna Mora",
+		"Area Code": "A",
+		"location_type": "Yoga",
+		"postcode": "1017VA"
+	},
+	{
+		"location_name": "Sukha Yoga",
+		"Area Code": "E",
+		"location_type": "Yoga",
+		"postcode": "1074CX"
+	},
+	{
+		"location_name": "SUP&Meer",
+		"Area Code": "E",
+		"location_type": "Overig",
+		"postcode": "1081KE"
+	},
+	{
+		"location_name": "Tennisvereniging Linnaeushof",
+		"Area Code": "C",
+		"location_type": "Overig",
+		"postcode": "1098KM"
+	},
+	{
+		"location_name": "The Company Sports",
+		"Area Code": "#N/A",
+		"location_type": "Fitness",
+		"postcode": "1119PB"
+	},
+	{
+		"location_name": "The Conscious Club",
+		"Area Code": "A",
+		"location_type": "Yoga",
+		"postcode": "1016RT"
+	},
+	{
+		"location_name": "The Movement Practice",
+		"Area Code": "D",
+		"location_type": "Yoga",
+		"postcode": "1053SZ"
+	},
+	{
+		"location_name": "The Temple Fitness",
+		"Area Code": "B",
+		"location_type": "Fitness",
+		"postcode": "1024KM"
+	},
+	{
+		"location_name": "The Wellness Garden",
+		"Area Code": "D",
+		"location_type": "Fitness",
+		"postcode": "1058GC"
+	},
+	{
+		"location_name": "Thrive Yoga",
+		"Area Code": "C",
+		"location_type": "Yoga",
+		"postcode": "1019BJ"
+	},
+	{
+		"location_name": "Thrive Yoga IJburg",
+		"Area Code": "C",
+		"location_type": "Yoga",
+		"postcode": "1087EN"
+	},
+	{
+		"location_name": "Thrive Yoga Oost",
+		"Area Code": "C",
+		"location_type": "Yoga",
+		"postcode": "1094LD"
+	},
+	{
+		"location_name": "Trainmore",
+		"Area Code": "A",
+		"location_type": "Fitness",
+		"postcode": "1016AB"
+	},
+	{
+		"location_name": "Trainmore De Pijp",
+		"Area Code": "E",
+		"location_type": "Fitness",
+		"postcode": "1072PC"
+	},
+	{
+		"location_name": "TULA yoga studio",
+		"Area Code": "A",
+		"location_type": "Yoga",
+		"postcode": "1013VD"
+	},
+	{
+		"location_name": "Vos Gym Amsterdam",
+		"Area Code": "D",
+		"location_type": "Vechtsport",
+		"postcode": "1051JI"
+	},
+	{
+		"location_name": "Vositivity Yoga",
+		"Area Code": "D",
+		"location_type": "Yoga",
+		"postcode": "1051LG"
+	},
+	{
+		"location_name": "Wellness 1926",
+		"Area Code": "C",
+		"location_type": "Overig",
+		"postcode": "1094RL"
+	},
+	{
+		"location_name": "WLADIMIR Dance Studios",
+		"Area Code": "A",
+		"location_type": "Dans",
+		"postcode": "1016VX"
+	},
+	{
+		"location_name": "Workout Amsterdam",
+		"Area Code": "D",
+		"location_type": "Bootcamp",
+		"postcode": "1053KL"
+	},
+	{
+		"location_name": "Y-Gym",
+		"Area Code": "C",
+		"location_type": "Bootcamp",
+		"postcode": "1087HW"
+	},
+	{
+		"location_name": "Yagoy | de Pijp",
+		"Area Code": "E",
+		"location_type": "Yoga",
+		"postcode": "1071SG"
+	},
+	{
+		"location_name": "Yoga Nieuw-West",
+		"Area Code": "D",
+		"location_type": "Yoga",
+		"postcode": "1065AS"
+	},
+	{
+		"location_name": "Yoga Point",
+		"Area Code": "B",
+		"location_type": "Yoga",
+		"postcode": "1032KV"
+	},
+	{
+		"location_name": "YogaFest Studio ",
+		"Area Code": "A",
+		"location_type": "Yoga",
+		"postcode": "1014BE"
+	},
+	{
+		"location_name": "Yogaschool Noord",
+		"Area Code": "B",
+		"location_type": "Yoga",
+		"postcode": "1021EZ"
+	},
+	{
+		"location_name": "Yogaspot",
+		"Area Code": "E",
+		"location_type": "Yoga",
+		"postcode": "1076DX"
+	},
+	{
+		"location_name": "Yung Hap",
+		"Area Code": "E",
+		"location_type": "Vechtsport",
+		"postcode": "1073TJ"
+	},
+	{
+		"location_name": "Zuiderbad",
+		"Area Code": "E",
+		"location_type": "Zwemmen",
+		"postcode": "1071ZC"
+	}
+];
+ 
+  var res = data.filter(item => { return item["Area Code"] == "A" && item["location_type"] == "Fitness"});
+  
+console.log(res);  
+
+output = {res};
